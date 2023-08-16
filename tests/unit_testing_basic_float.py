e5d274304c318764d1280620bb06a1ff303525ea
Riska Amalia <amaliariska8@yahoo.co.id>
2023-08-16 12:00:11 +0700

    feat: add prototype feature testing float

---
+++ tests/unit_testing_basic_float.py
@@ -0,0 +1,59 @@
+import unittest
+import pandas
+
+'''
+A unit testing of pandas dataframe data quality in float data type
+'''
+
+
+class DFFloatTests(unittest.TestCase):
+
+    """ class for running unittests of float data type
+            Args:
+                    data (pd.DataFrame): data
+                    column_names: list"""
+
+    # this is a mandatory function
+    # fill column_names_checked if you want to check column
+    # fill row_index_checked if you want to check row
+    # default: all cells in dataframe is checked
+    def __init__(
+        self,
+        data: pandas.DataFrame,
+        column_names_checked: list,
+        row_index_checked: list,
+    ):
+        self.data = data
+        self.column_names_checked = column_names_checked
+        self.row_index_checked = row_index_checked
+
+    # this is a mandatory function
+    # checking parameter is a pandas Dataframe or not
+    def test_is_dataframe(self):
+        # real result test in variable
+        test_data = pandas.DataFrame()
+        print(type(test_data))
+        # call the module here
+        return False
+
+    # this is a mandatory function
+    # checking parameter is empty dataframe or not
+    def test_is_empty_dataframe(self):
+        return False
+
+    # this is a mandatory function
+    # checking parameter is empty row dataframe or not
+    def test_is_empty_dataframe_row(self):
+        return False
+
+    # this is a mandatory function
+    # checking parameter is empty column dataframe or not
+    def test_is_empty_dataframe_column(self):
+        return False
+
+    # checking is dataframe, row or column is float datatype
+    def test_is_float_datatype(self):
+        return False
+
+    def test_range(self):
+        self.assertEqual(4, 4)
